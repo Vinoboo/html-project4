2ba73afb6baff598f8d91996951ed252e5050df6
Vinoboo <raja2003boopathi@gmail.com>
2024-07-09 23:29:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById("1st month").addEventListener('click',function(){
+    document.getElementById("image").src="https://media.istockphoto.com/id/1257580623/photo/hand-and-people-with-body-part.jpg?s=1024x1024&w=is&k=20&c=sa_DCNu4vslqsUY4jwithsK9XSDJZaT6oWH8xSZl1Ws="});
+document.getElementById("2nd month").addEventListener('click',function(){
+    document.getElementById("image").src="https://media.istockphoto.com/id/1931861318/photo/mri-of-the-upper-abdomen-axial-view-is-a-non-invasive-imaging-technique-providing-detailed.jpg?s=1024x1024&w=is&k=20&c=HnztUIR6WHzM1PaxOXSqqNGaS7p4On1D-1hsTOC-FbQ="});
+document.getElementById("3rd month").addEventListener('click',function(){
+    document.getElementById("image").src="https://media.istockphoto.com/id/1064299270/photo/photos-of-the-child-in-the-12-week-of-pregnancy-ultrasound-procedure-screening-in-the-first.jpg?s=1024x1024&w=is&k=20&c=_G4feuQKPuLi7kl_XgjZTVdUQFout8tBDi9r-M6rBSs="});
+document.getElementById("4th month").addEventListener('click',function(){
+    document.getElementById("image").src="https://media.istockphoto.com/id/963194728/photo/identical-twin-boy-obstetrical-ultrasound.jpg?s=1024x1024&w=is&k=20&c=FUpZtu_VoQOqTCk3d-NfLOP3bRK0Xlg4yrZ1Els5DtQ=" });
+document.getElementById("5th month").addEventListener('click',function(){
+    document.getElementById("image").src="https://media.istockphoto.com/id/1315736599/photo/12-weeks.jpg?s=1024x1024&w=is&k=20&c=Lg4lNqDO0TXGUe0rhIYpqElaaz6o1nQkxrQrcXTHflI="});
+document.getElementById("6th month").addEventListener('click',function(){
+    document.getElementById("image").src="https://media.istockphoto.com/id/517677164/photo/9th-week-fetus.jpg?s=1024x1024&w=is&k=20&c=CO9Hx2_moAaKP3VwTZgeXj2plKqLejzt1kGVHd9ofNY=" });
+document.getElementById("7th month").addEventListener('click',function(){
+    document.getElementById("image").src="https://media.istockphoto.com/id/184281351/photo/fetus-ultrasound.jpg?s=1024x1024&w=is&k=20&c=KYgu3gnYBH-H7i70WpGGUNIFp3eW5pDdnrxCoMAp7MQ="});
+document.getElementById("8th month").addEventListener('click',function(){
+    document.getElementById("image").src="https://media.istockphoto.com/id/172250396/photo/a-ultra-sound-picture-of-a-baby.jpg?s=1024x1024&w=is&k=20&c=6LeHj2Birap0vkK0wRbWRvVjD6FhKqOgVRlmXNdTkWk="});
+document.getElementById("9th month").addEventListener('click',function(){
+    document.getElementById("image").src="https://media.istockphoto.com/id/1064299270/photo/photos-of-the-child-in-the-12-week-of-pregnancy-ultrasound-procedure-screening-in-the-first.jpg?s=1024x1024&w=is&k=20&c=_G4feuQKPuLi7kl_XgjZTVdUQFout8tBDi9r-M6rBSs=" });
+document.getElementById("10th month").addEventListener('click',function(){
+    document.getElementById("image").src="https://media.istockphoto.com/id/1197512022/photo/ultrasound-scan-of-a-child-collage-different-angles.jpg?s=1024x1024&w=is&k=20&c=uvdXPa-lYOSCMHZ8C2D0-9bRBH436UIdU5fMqa1vVzw="});
